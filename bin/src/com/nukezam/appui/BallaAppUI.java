ef44b5d67003247edb8b743ef5c4d6488fcb3938
Daylightmazkeun <ma.zekun@qq.com>
2018-12-01 15:10:40 +0800

    rebuild project
    rebuild project

---
+++ bin/src/com/nukezam/appui/BallaAppUI.java
@@ -0,0 +1,18 @@
+package com.nukezam.appui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class BallaAppUI {
+	public static void main(String[] args) {
+		JFrame jFrame = new JFrame();
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(null);
+		jFrame.setContentPane(contentPane);
+		// TODO
+        jFrame.setUndecorated(false);
+        jFrame.setVisible(true);
+        jFrame.setSize(300, 250);
+        jFrame.setLocationRelativeTo(null);
+	}
+}
